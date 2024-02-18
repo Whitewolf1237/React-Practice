b0c1be6b38a2cd3fa6874a700c373a10b06cacc7
Whitewolf1237 <132841363+Whitewolf1237@users.noreply.github.com>
2024-02-18 14:33:55 +0530

    1

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import React from "react";
+import { render } from "react-dom";
 
 
-/**
- * 
- * <div id="parent">
- *      <div id="child">
- *          <h1>HEading Tag</h1>
- *      </div>
- * </div>
- */
+const heading=React.createElement("h1",{},"Heading1")
 
+const root=ReactDOM.createRoot(document.getElementById("root"))
 
-const parent = React.createElement("div",{id:"parent"},
-                React.createElement("div",{id:"child"},
-                [React.createElement("h1",{},"This is H1 tag"),React.createElement("h2",{},"This is H2 tag")]
-                )
-)
-
-// const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World")
-// (element name,{id,class,etc},text to be inserted)
-
-// this will return an object 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent);
+root.render(heading)
