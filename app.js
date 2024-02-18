aac3c60cd81ca0f9c7870a3a93e0bb9c337252c1
Whitewolf1237 <132841363+Whitewolf1237@users.noreply.github.com>
2024-02-18 15:48:25 +0530

    2nd

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ import React from "react";
 import { render } from "react-dom";
 
 
-const heading=React.createElement("h1",{},"Heading1")
+// const Heading=<h1 className="head" tabIndex="5">HEADING THROUGH JSX</h1>
+
+//functional Component
+
+const Title=()=>
+    (   
+            <h1 className="heading">Title component</h1>
+    );
+const HeadingComponent=()=>
+    (   
+        <div id="container">
+            <Title />
+            <h1 className="heading">Functional Component test case</h1>
+        </div>
+    );
 
 const root=ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading)
+
+
+
+
+// to render react component we will <Component_name />
+root.render(<HeadingComponent />)
