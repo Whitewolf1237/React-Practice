c82a045f81260bd9fa85a070b91bd80bc35b2122
Whitewolf1237 <132841363+Whitewolf1237@users.noreply.github.com>
2024-02-20 12:05:44 +0530

    .

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ const Header=()=>{
 
 
 
-const Restaurant_card=()=>{
+const Restaurant_card=(props)=>{
     return (
         <div className="res-card" style={{ backgroundColor:"#f0f0f0"}}>
-            <img className="res-logo" alt="logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ldgjutok2ha7asc05p0q" />
-            <h3>Meghna Foods</h3>
-            <h4>Chole,North Indian</h4>
-            <h4>4.4 stars</h4>
+            <img className="res-logo" src = {props.reslogo} alt = "Restaurant Logo" />
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>{props.rating}</h4>
             <h4>38 mins</h4>
         </div>
     )
@@ -39,14 +39,14 @@ const Body=()=>{
         <div className="body">
             <div className="Search">Search</div>
             <div className="res-container">
-                <Restaurant_card />
-                <Restaurant_card />
-                <Restaurant_card />
-                <Restaurant_card />
-                <Restaurant_card />
-                <Restaurant_card />
-                <Restaurant_card />
-                <Restaurant_card />
+                <Restaurant_card reslogo="https://source.unsplash.com/1600x900/?food&1" resName="Meghna Food" cuisine="Chole,North Indian" rating="4.4⭐" />
+                <Restaurant_card reslogo="https://source.unsplash.com/1600x900/?food&2" resName="KFC" cuisine="Burger,Fast Food" rating="4.7⭐" />
+                <Restaurant_card reslogo="https://source.unsplash.com/1600x900/?coffee&3" resName="Starbucks" cuisine="coffee" rating="4.8⭐"/>
+                <Restaurant_card reslogo="https://source.unsplash.com/1600x900/?coffee&4" resName="NecCafe" cuisine="coffee" rating="4.7⭐" />
+                <Restaurant_card reslogo="https://source.unsplash.com/1600x900/?pizza&5" resName="PizzaHut" cuisine="Pizza" rating="4.9⭐" />
+                <Restaurant_card reslogo="https://source.unsplash.com/1600x900/?pizza&6" resName="Dominos" cuisine="Pizza" rating="4.8⭐" />
+                <Restaurant_card reslogo="https://source.unsplash.com/1600x900/?food&7" resName="Mamta Foods" cuisine="Street Food" rating="4.6⭐" />
+                <Restaurant_card reslogo="https://source.unsplash.com/1600x900/?food&8" resName="Bru" cuisine="coffee" rating="4.8⭐" />
             </div>
         </div>
     )
