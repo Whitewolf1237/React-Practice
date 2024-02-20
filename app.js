5d207f0cc7bed53f6b9ac9b261f542dc84183f44
Whitewolf1237 <132841363+Whitewolf1237@users.noreply.github.com>
2024-02-20 11:07:01 +0530

    .

--- app.js
+++ app.js
@@ -20,10 +20,16 @@ const Header=()=>{
     )
 }
 
+
+
 const Restaurant_card=()=>{
     return (
-        <div className="res-card">
-            <h3>Meghna</h3>
+        <div className="res-card" style={{ backgroundColor:"#f0f0f0"}}>
+            <img className="res-logo" alt="logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ldgjutok2ha7asc05p0q" />
+            <h3>Meghna Foods</h3>
+            <h4>Chole,North Indian</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 mins</h4>
         </div>
     )
 }
@@ -33,6 +39,13 @@ const Body=()=>{
         <div className="body">
             <div className="Search">Search</div>
             <div className="res-container">
+                <Restaurant_card />
+                <Restaurant_card />
+                <Restaurant_card />
+                <Restaurant_card />
+                <Restaurant_card />
+                <Restaurant_card />
+                <Restaurant_card />
                 <Restaurant_card />
             </div>
         </div>
