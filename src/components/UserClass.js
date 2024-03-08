2c32b62f4502eea9d1b7498c190429a3bab92b27
Whitewolf1237 <132841363+Whitewolf1237@users.noreply.github.com>
2024-03-08 14:42:24 +0530

    k

---
+++ src/components/UserClass.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class UserClass extends React.Component{
+    render(){
+        return(
+            <div className="user-card">
+                <h2>Name:Nitin Kumar Rai</h2>
+                <h3>Location:Noida</h3>
+                <h3>Contact:aiml21041@glbitm.ac.in</h3>
+            </div>
+    )
+}
+}
+
+export default UserClass;
